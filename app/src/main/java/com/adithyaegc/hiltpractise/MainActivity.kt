fcac9a6b5f7df8a9111619d3d0a7bd69a1fd1162
Adithya Egc <adithyarang@gmail.com>
2021-08-26 13:00:17 +0530

    Inside new Branch

--- app/src/main/java/com/adithyaegc/hiltpractise/MainActivity.kt
+++ app/src/main/java/com/adithyaegc/hiltpractise/MainActivity.kt
@@ -9,7 +9,7 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
 
         println("This is one more commit")
-    }
 
-    one more time
+        println("Inside the New Branch")
+    }
 }
